01245b3e8db0efa09f6f9ef982a28dafee163515
yaya-richard <zhjy0207@163.com>
2023-10-02 00:11:20 +0800

    增加clear函数和调用接口

--- src/clear/clear.py
+++ src/clear/clear.py
@@ -1,20 +1,9 @@
-
 import os
 import shutil
 
-OUTPUT_ROOT = "./output"
+from src.core.generator import Generator, GenerateType
 
-COUNT_FILE = "./count.txt"
-PUB_COUNT_FILE = "./pub_count.txt"
+clearer = Generator(GenerateType.Video)
 
 if __name__ == '__main__':
-
-    with open(COUNT_FILE, "w", encoding="utf8") as file:
-        file.write("0")
-    with open(PUB_COUNT_FILE, "w", encoding="utf8") as file:
-        file.write("0")
-
-    shutil.rmtree(OUTPUT_ROOT)
-    os.mkdir(OUTPUT_ROOT)
-
-# TODO: [君扬] 给Generator添加一个clear函数，用于清除生成的文件，该脚本的作用在于调用这个clear函数
+    clearer.clear()
